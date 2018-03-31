8e453375f44267e6643f59433ba38b9b5408164e
weishao6hao <798364373@qq.com>
2018-03-31 12:59:35 +0800

    插入排序

---
+++ 常用排序算法/插入排序.py
@@ -0,0 +1,12 @@
+def insert_sort(lists):
+    count = len(lists)
+    for i in range(1, count):
+        key = lists[i]
+        j = i - 1
+        while j >= 0:
+            if lists[j] > key:
+                lists[j + 1] = lists[j]
+                lists[j] = key
+            j -= 1
+    return lists
+inser_sort([2,5,7,9,2,4,,6,8,32])

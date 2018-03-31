5e7971cf81d498e28555603ebef79f56f66de2e9
weishao6hao <798364373@qq.com>
2018-03-31 12:59:26 +0800

    快速排序

---
+++ 常用排序算法/快速排序.py
@@ -0,0 +1,18 @@
+def quick_sort(lists, left, right):
+    if left >= right:
+        return lists
+    key = lists[left]
+    low = left
+    high = right
+    while left < right:
+        while left < right and lists[right] >= key:
+            right -= 1
+        lists[left] = lists[right]
+        while left < right and lists[left] <= key:
+            left += 1
+        lists[right] = lists[left]
+    lists[right] = key
+    quick_sort(lists, low, left - 1)
+    quick_sort(lists, left + 1, high)
+    return lists
+quick_sort([2,5,7,9,2,4,,6,8,32])

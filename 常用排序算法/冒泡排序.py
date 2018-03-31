6ba41e9217b3dac196c2931a64e7c26bd5d0e7d1
weishao6hao <798364373@qq.com>
2018-03-31 12:58:50 +0800

    冒泡排序

---
+++ 常用排序算法/冒泡排序.py
@@ -0,0 +1,8 @@
+def bubble_sort(lists):
+    count = len(lists)
+    for i in range(0, count):
+        for j in range(i + 1, count):
+            if lists[i] > lists[j]:
+                lists[i], lists[j] = lists[j], lists[i]
+    return lists
+bubble_sort([2,5,7,9,2,4,,6,8,32])
